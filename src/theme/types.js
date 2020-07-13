c457e0483344498406784ca1e7ce0fdf873fbc01
OreAkintobi <oreakintobi@gmail.com>
2020-07-13 15:36:38 +0100

    add fonts + colors to theme files

---
+++ src/theme/types.js
@@ -0,0 +1,32 @@
+const fonts = {
+  FUTURA_MD_BOLD: "FuturaMD-Bold",
+  FUTURA_MD_BOLD_ITALIC: "FuturaMD-Bold-Italic",
+  FUTURA_MD_LIGHT: "FuturaMD-Light",
+  FUTURA_MD_MEDIUM: "FuturaMD-Medium",
+  FUTURA_MD_MEDIUM_ITALIC: "FuturaMD-Medium-Italic",
+};
+
+const sizes = {
+  FONT_SMALL_SIZE: 10,
+  FONT_MEDIUM_SIZE: 12,
+  FONT_LARGE_SIZE: 15,
+};
+
+const colors = {
+  BG_OFF_WHITE: "#E5E5E5",
+  DARK_TEXT: "#000000",
+  LIGHT_GRAY: "#EAEAEA",
+  MEDIUM_GRAY: "#979797",
+  DARK_GRAY: "#6D6D6D",
+  BLUE_LINK_TEXT: "#002FD3",
+  COLOR_WHITE: "#FFFFFF",
+  LIGHT_GREEN: "#00FF44",
+  LIGHT_RED: "#FF0000",
+  DARK_RED: "#6C1111",
+  OFF_WHITE: "#FFFDFD",
+  PURPLE: "#3a2c85",
+  DARK_COLOR_LOW_OPACITY: "rgba(0, 0, 0, 0.1)",
+  DARK_COLOR_HIGH_OPACITY: "rgba(0, 0, 0, 0.88);",
+};
+
+export const theme = { colors, sizes, fonts };
