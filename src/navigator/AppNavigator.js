12815857dacce199bf8c17603cba4dc47f4d3550
OreAkintobi <oreakintobi@gmail.com>
2020-07-13 15:51:36 +0100

    create AppNavigator component

---
+++ src/navigator/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Screens from "../screens";
+import { theme } from "../theme/types";
+import BottomNavigator from "./BottomNavigator";
+
+const AppStack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <AppStack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{ headerShown: false }}
+    >
+      <AppStack.Screen name="HomeScreen" component={BottomNavigator} />
+    </AppStack.Navigator>
+  );
+}
