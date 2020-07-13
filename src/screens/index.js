691ca94ec35d25b780a3b49c8e113e68e3491db6
OreAkintobi <oreakintobi@gmail.com>
2020-07-13 15:48:56 +0100

    create screens file to manage screens

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import HomeScreen from "./homescreen";
+import SplashScreen from "./splashscreen";
+import MenuScreen from "./menuscreen";
+
+export default {
+  HomeScreen,
+  SplashScreen,
+  MenuScreen,
+};
